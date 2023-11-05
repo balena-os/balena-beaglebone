1ce4a67dda54fabd68440d9c5268b431beffc22f
Jason Kridner <jkridner@beagleboard.org>
2023-11-05 03:44:08 -0500

    Match kernel and HostOS -bn2 in version

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 HERE="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+COMMIT=144b01a94e5604a6d59e2d1680af6e1730ad1847
+VERSION=4.0.1-bn2
 balena login
 $HERE/balena-yocto-scripts/build/balena-build.sh -d beaglebone -t balena-image-flasher -k -s $HERE/work
-balena os configure $HERE/build/tmp/deploy/images/beaglebone/balena-image-flasher-beaglebone.balenaos-img -f v3-gateway --device-type beaglebone --config $HERE/bn-customizations/config.json --system-connection $HERE/bn-customizations/system-connections/* --version 4.0.1+bn1 --config-network ethernet
-balena preload $HERE/build/tmp/deploy/images/beaglebone/balena-image-flasher-beaglebone.balenaos-img --fleet v3-gateway --commit be5f74c7cb3a265f2a719d234998fe0db1d0cc1a -p
+balena os configure $HERE/build/tmp/deploy/images/beaglebone/balena-image-flasher-beaglebone.balenaos-img -f v3-gateway --device-type beaglebone --config $HERE/bn-customizations/config.json --system-connection $HERE/bn-customizations/system-connections/* --version $VERSION --config-network ethernet
+balena preload $HERE/build/tmp/deploy/images/beaglebone/balena-image-flasher-beaglebone.balenaos-img --fleet v3-gateway --commit $COMMIT -p
+docker load -i build/tmp/deploy/images/beaglebone/balena-image-beaglebone.docker | \
+ sed -n 's/^Loaded image ID: sha256:\([0-9a-f]*\).*/\1/p' | \
+ xargs -i docker tag {} jkridner/beaglebone-image:$VERSION
+docker push jkridner/beaglebone-image:$VERSION
